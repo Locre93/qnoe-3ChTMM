544e10870d8a39547e2cb4a5dc2f21dda626aca9
Lorenzo Orsini <lorenzo.orsini@icfo.eu>
2023-09-26 10:22:19 +0200

    Simulation of graphene plasmons fringes - near-field scan

--- examples/Example 02.py
+++ examples/Example 02.py
@@ -2,28 +2,31 @@ from qnoe_3ChTMM import *
 import matplotlib.pyplot as plt
 
 # Section 4. Simulation of graphene plasmons
-# Main text
+# Constants
+μm = 1e-6
 
+# Graphene
 Sheet = Graphene()
 
-Ef = [0.1,0.2,0.3,0.4]				# Fermi energy [eV]
-k = np.arange(100,350,0.1) 			# Wavenumber [cm⁻¹] - 1/λ (4.497THz to 10.493THz)
-ϵ = [1,1]							# Dielectric permittivities of the environment [Above,Below]
+Ef = 0.4														# Fermi energy [eV]
+ϵ = [1,1]														# Dielectric permittivities of the environment [Above,Below]
+k = np.arange(100,350,0.1) 										# Wavenumber [cm⁻¹] - 1/λ (4.497THz to 10.493THz)
+ϵ_eff = np.real(Sheet.ModeEffectivePermittivity(k,Ef,ϵ))	
 
+# Structure initialization
+SHEET = Chunk(name="Suspended graphene",k=k,ϵ=ϵ_eff,length=10,units=μm)
+BOUNDARY = Interface(name="Boundary",k=k).set(ReflectingInterface(k=k,ϕ=0))
 
-ϵ_eff = np.zeros(shape = (len(k),len(Ef)), dtype = complex)
-Q = np.zeros(shape = (len(k),len(Ef)), dtype = complex)
-λp = np.zeros(shape = (len(k),len(Ef)), dtype = complex)
+# System initialization
+system = TMM_sSNOM_Simple([SHEET,BOUNDARY],position=1,site=1,units=μm)
 
-for i in range(len(Ef)):
-	Q[:,i] = Sheet.ModeWavevector(k,Ef[i],ϵ)							# Plasmon wavevector [m⁻¹]
-	ϵ_eff[:,i] = np.real(Sheet.ModeEffectivePermittivity(k,Ef[i],ϵ))	# Plasmon's effective permittivity
-	λp[:,i] = 2*np.pi/np.real(Q[:,i])									# Plasmon wavelength [m]
+# Near-field response calculation
+x,MAP = system.Scan(sites=[1],resolution=0.1,units=μm)
+X,K = np.meshgrid(x,k)
 
-SHEET = Chunk("Ef 0.4eV",k,ϵ_eff[:,3],10,1e-6)
-
-BOUNDARY = Interface("",k)
-BOUNDARY.set(ReflectingInterface(k=k,ϕ=0))
-
-system = TMM_sSNOM_Simple([SHEET,BOUNDARY],position=1,site=1,units=1e-6)
+plt.contourf(X,K,np.abs(MAP),100)
+plt.xlabel('X, μm',size=16)
+plt.ylabel('Wavenumber, cm⁻¹',size=16)
+plt.title('Near-field',size=16)
+plt.show()
 
