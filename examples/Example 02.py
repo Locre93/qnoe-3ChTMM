213b33ab004129b916f7827283a7abe97e7683c8
Lorenzo Orsini <lorsini@icfo.net>
2023-10-02 18:15:55 +0200

    updated classes and functions names

--- examples/Example 02.py
+++ examples/Example 02.py
@@ -11,19 +11,19 @@ k = np.arange(1400,1560,1)		# Wavenumber [1/cm] - 1/λ
 thickness = 38   	# hBN thickness [nm]
 isotope = 11 		# hBN isotope
 
-hBN = hexagonalBoronNitride(isotope,thickness,nm)			# Creation of an instance of hexagonalBoronNitride class
-M1 = hBN.ModeEffectivePermittivity(k, 1, [1,-10000])		# Calculation of the effective dielectric permittivity ϵ for the mode M1 (Real part is chosen to simulate a lossless system)
+hBN = HexagonalBoronNitride(isotope,thickness,nm)			# Creation of an instance of hexagonalBoronNitride class
+M1 = hBN.mode_effective_permittivity(k, 1, [1,-10000])		# Calculation of the effective dielectric permittivity ϵ for the mode M1 (Real part is chosen to simulate a lossless system)
 
 # Structure initialization
 CHUNK_M1 = Chunk("M1",k,M1,1600,nm)
-BOUNDARY = Interface(name="Boundary",k=k).set(ReflectingInterface(k=k,ϕ=120*np.pi/180))		# Phese ϕ upon reflection
+BOUNDARY = Interface(name="Boundary",k=k).set(reflecting_interface(k=k,ϕ=120*np.pi/180))		# Phese ϕ upon reflection
 
 Flake_Edge = TMM_sSNOM_Advanced([CHUNK_M1,BOUNDARY],position=800,site=1,units=nm)
 
 sites = [1]
 resolution = 10
 
-x,MAP = Flake_Edge.Scan(sites=sites,resolution=resolution)
+x,MAP = Flake_Edge.scan(sites=sites,resolution=resolution)
 X,K = np.meshgrid(x,k)
 
 plt.contourf(X,K,np.abs(MAP),100)
